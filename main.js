3652c5a1509bdc4f1e048f61a707017edbe1ded8
miszter83 <miszter83@gmail.com>
2023-04-24 20:42:45 +0200

    hambuger menü JS fejlesztése

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 let button = document.getElementById("nav-checkbox");
 let OpenC = document.querySelector(".nav-list");
 
-button.addEventListener("click", Hmenu);
+button.addEventListener("change", Hmenu);
 OpenC.addEventListener("click", Hmenu);
+
+OpenC.addEventListener("click", CheckReset);
+
+function CheckReset() {
+  button.checked = !button.checked;
+}
 
 function Hmenu() {
   OpenC.classList.toggle("active");
